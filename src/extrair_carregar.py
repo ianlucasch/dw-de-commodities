868568386cc4f70590ba4b51628b25cbef79c53e
ianlucasch <ianlucaschaves@gmail.com>
2024-07-01 12:24:35 -0300

    Extração dos dados e conexão com banco de dados

---
+++ src/extrair_carregar.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+commodities = ['CL=F', 'GC=F', 'SI=F']
+
+load_dotenv(".env")
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+def buscar_dados_commodities(simbolo, periodo='1mo', intervalo='1d'):
+    ticker = yf.Ticker(simbolo)
+    dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
+    dados['Simbolo'] = simbolo
+    return dados
+
+def buscar_todos_dados_commodities(commodities):
+    todos_dados = []
+    for simbolos in commodities:
+        dados = buscar_dados_commodities(simbolos)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
+
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
+if __name__ == "__main__":
+    dados_concatenados = buscar_todos_dados_commodities(commodities)
+    salvar_no_postgres(dados_concatenados, schema='public')
